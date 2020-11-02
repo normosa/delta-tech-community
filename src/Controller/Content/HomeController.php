d072a182ef104f693d18412d95faf1cf37fef12f
Norman Osaruyi <norman@skysenx.com>
2020-11-02 21:44:07 +0100

    home page setup

---
+++ src/Controller/Content/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller\Content;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController {
+    
+    /**
+     * @Route("/")
+     */
+    public function index():Response {
+        return $this->render('content/home.html.twig', [
+            'title' => 'Delta Tech Community',
+        ]);
+    }
+}
